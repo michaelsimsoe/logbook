6df0aa265a877f5fd173b2600b62bf3252e2f63f
Michael Krøyserth-Simsø <michael.simso@visma.com>
2021-05-12 23:37:22 +0200

    test: explosion in production

--- resources/views/components/notes/note-tag.blade.php
+++ resources/views/components/notes/note-tag.blade.php
@@ -1,3 +1,3 @@
-<span style="border: 2px #{{$color}} solid" class="rounded-lg mr-2 px-1 bg-gray-200 text-gray-600 shadow">
+<span style="border: 2px red solid" class="rounded-lg mr-2 px-1 bg-gray-200 text-gray-600 shadow">
     {{ $name }}
 </span>
