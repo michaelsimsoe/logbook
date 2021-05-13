f8d76ae985b71af439d1dcb9f9e6262c81cc3ebd
Michael Krøyserth-Simsø <michael.simso@visma.com>
2021-05-13 10:28:14 +0200

    test: commiting breaking change

--- resources/views/components/notes/note-tag.blade.php
+++ resources/views/components/notes/note-tag.blade.php
@@ -1,6 +1,6 @@
 {{-- <span style="border: 2px #{{ $color }} solid" class="rounded-lg mr-2 px-1 bg-gray-200 text-gray-600 shadow">
     {{ $name }}
 </span> --}}
-<span style="border: 2px  solid" class="rounded-lg mr-2 px-1 bg-gray-200 text-gray-600 shadow">
+<span style="border: 2px #{{ $color }} solid" class="rounded-lg mr-2 px-1 bg-gray-200 text-gray-600 shadow">
     {{ $name }}
 </span>
